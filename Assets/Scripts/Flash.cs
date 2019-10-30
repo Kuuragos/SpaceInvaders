9466f661eb6faa20178b764ff3e5ccc266b72c4d
Kuuragos <Alejandrab.19401@gmail.com>
2019-10-30 15:22:09 -0500

    assets

---
+++ Assets/Scripts/Flash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flash : MonoBehaviour
+{
+    public GameObject flash1;
+    public GameObject flash2;
+
+    void Start()
+    {
+        flash1.SetActive(false);
+        flash2.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.Mouse0))
+        {
+            flash1.SetActive(true);
+            flash2.SetActive(true);
+        }
+        else
+        {
+            flash1.SetActive(false);
+            flash2.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    float speed = 0.1f;
+    void Update()
+    {
+       if(Input.GetKey(KeyCode.W) && transform.position.y < 0)
+        {
+            transform.position += transform.forward * speed;
+        }
+       
+       if (Input.GetKey(KeyCode.S) && transform.position.y > -5)
+        {
+            transform.position -= transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -7.07)
+        {
+            transform.position -= Vector3.right * speed;
+            transform.rotation = Quaternion.Euler(new Vector3(-90, 35, 5));
+        }
+        else if (Input.GetKey(KeyCode.D) && transform.position.x < 7.07)
+        {
+            transform.position += Vector3.right * speed;
+            transform.rotation = Quaternion.Euler(new Vector3(-90, -35, -5));
+        }
+        else
+            transform.rotation = Quaternion.Euler(new Vector3(-90, 0, 0));
+    }
+}
